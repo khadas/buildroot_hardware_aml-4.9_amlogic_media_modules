04fd3e6f8ad888ccb8a3f38369010e69bb6d595a
Nanxin Qin <nanxin.qin@amlogic.com>
2017-09-08 11:28:41 +0800

    media_modules: merged code from 43177e6a on the amlogic-3.14-dev [2/3]
    PD#150739: merged code from 43177e6a on the amlogic-3.14-dev
    1.decoder: vmh264, fix multi pic in one packet
    2.decoder: add amvdec_stop when error reset
    3.clk: switch vdec clk source when suspend
    
    Change-Id: I43b8f5a7a13d880c130505cd21e08c8e1eb4cf38
    Signed-off-by: Nanxin Qin <nanxin.qin@amlogic.com>

--- drivers/frame_provider/decoder/vp9/vvp9.c
+++ drivers/frame_provider/decoder/vp9/vvp9.c
@@ -362,7 +362,7 @@ typedef unsigned short u16;
 #endif
 
 static u32 debug;
-
+static bool is_reset;
 /*for debug*/
 /*
 	udebug_flag:
@@ -1314,13 +1314,14 @@ struct VP9Decoder_s {
 	int new_frame_displayed;
 	void *mmu_box;
 	void *bmmu_box;
+	struct vframe_master_display_colour_s vf_dp;
 	struct firmware_s *fw;
 } VP9Decoder;
 
 static int vp9_print(struct VP9Decoder_s *pbi,
 	int flag, const char *fmt, ...)
 {
-#define HEVC_PRINT_BUF		128
+#define HEVC_PRINT_BUF		256
 	unsigned char buf[HEVC_PRINT_BUF];
 	int len = 0;
 	if (pbi == NULL ||
@@ -1391,7 +1392,7 @@ static int get_double_write_mode(struct VP9Decoder_s *pbi)
 		pbi->double_write_mode : double_write_mode;
 }
 #endif
-
+#define	MAX_4K_NUM		0x1200
 #ifdef VP9_10B_MMU
 int vp9_alloc_mmu(
 	struct VP9Decoder_s *pbi,
@@ -1408,6 +1409,11 @@ int vp9_alloc_mmu(
 	picture_size = compute_losless_comp_body_size(pic_width, pic_height,
 				   bit_depth_10);
 	cur_mmu_4k_number = ((picture_size + (1 << 12) - 1) >> 12);
+	if (cur_mmu_4k_number > MAX_4K_NUM) {
+		pr_err("over max !! cur_mmu_4k_number 0x%x width %d height %d\n",
+			cur_mmu_4k_number, pic_width, pic_height);
+		return -1;
+	}
 	return decoder_mmu_box_alloc_idx(
 		pbi->mmu_box,
 		cur_buf_idx,
@@ -5136,9 +5142,6 @@ static int vp9_local_init(struct VP9Decoder_s *pbi)
 	pts_unstable = ((unsigned long)(pbi->vvp9_amstream_dec_info.param)
 			& 0x40) >> 6;
 
-	pbi->video_signal_type = 0;
-	video_signal_type = pbi->video_signal_type;
-
 	if ((debug & VP9_DEBUG_SEND_PARAM_WITH_REG) == 0) {
 		pbi->rpm_addr = kmalloc(RPM_BUF_SIZE, GFP_KERNEL);
 		if (pbi->rpm_addr == NULL) {
@@ -5336,6 +5339,8 @@ static void set_frame_info(struct VP9Decoder_s *pbi, struct vframe_s *vf)
 	vf->duration = pbi->frame_dur;
 	vf->duration_pulldown = 0;
 	vf->flag = 0;
+	vf->prop.master_display_colour = pbi->vf_dp;
+	vf->signal_type = pbi->video_signal_type;
 
 	ar = min_t(u32, pbi->frame_ar, DISP_RATIO_ASPECT_RATIO_MAX);
 	vf->ratio_control = (ar << DISP_RATIO_ASPECT_RATIO_BIT);
@@ -6444,6 +6449,12 @@ int vvp9_dec_status(struct vdec_s *vdec, struct vdec_info *vstatus)
 	return 0;
 }
 
+int vvp9_set_isreset(struct vdec_s *vdec, int isreset)
+{
+	is_reset = isreset;
+	return 0;
+}
+
 #if 0
 static void VP9_DECODE_INIT(void)
 {
@@ -6667,9 +6678,9 @@ static s32 vvp9_init(struct VP9Decoder_s *pbi)
 					 pbi);
 	vf_reg_provider(&vvp9_vf_prov);
 	vf_notify_receiver(PROVIDER_NAME, VFRAME_EVENT_PROVIDER_START, NULL);
-
-	vf_notify_receiver(PROVIDER_NAME, VFRAME_EVENT_PROVIDER_FR_HINT,
-	(void *)((unsigned long)pbi->frame_dur));
+	if (!is_reset)
+		vf_notify_receiver(PROVIDER_NAME, VFRAME_EVENT_PROVIDER_FR_HINT,
+		(void *)((unsigned long)pbi->frame_dur));
 
 	pbi->stat |= STAT_VF_HOOK;
 
@@ -6720,8 +6731,10 @@ static int vvp9_stop(struct VP9Decoder_s *pbi)
 	}
 
 	if (pbi->stat & STAT_VF_HOOK) {
-		vf_notify_receiver(pbi->provider_name,
-				VFRAME_EVENT_PROVIDER_FR_END_HINT, NULL);
+		if (!is_reset)
+			vf_notify_receiver(pbi->provider_name,
+					VFRAME_EVENT_PROVIDER_FR_END_HINT,
+					NULL);
 
 		vf_unreg_provider(&vvp9_vf_prov);
 		pbi->stat &= ~STAT_VF_HOOK;
@@ -6853,6 +6866,8 @@ static int amvdec_vp9_probe(struct platform_device *pdev)
 	cma_dev = pdata->cma_dev;
 #endif
 	pdata->dec_status = vvp9_dec_status;
+	pdata->set_isreset = vvp9_set_isreset;
+	is_reset = 0;
 
 	if (vvp9_init(pbi) < 0) {
 		pr_info("\namvdec_vp9 init failed.\n");
@@ -7398,6 +7413,8 @@ static int ammvdec_vp9_probe(struct platform_device *pdev)
 	struct vdec_s *pdata = *(struct vdec_s **)pdev->dev.platform_data;
 	int ret;
 	int config_val;
+	struct vframe_content_light_level_s content_light_level;
+	struct vframe_master_display_colour_s vf_dp;
 
 	struct BUF_s BUF[MAX_BUF_NUM];
 	struct VP9Decoder_s *pbi = NULL;
@@ -7408,6 +7425,7 @@ static int ammvdec_vp9_probe(struct platform_device *pdev)
 	}
 	/*pbi = (struct VP9Decoder_s *)devm_kzalloc(&pdev->dev,
 		sizeof(struct VP9Decoder_s), GFP_KERNEL);*/
+	memset(&vf_dp, 0, sizeof(struct vframe_master_display_colour_s));
 	pbi = vmalloc(sizeof(struct VP9Decoder_s));
 	memset(pbi, 0, sizeof(struct VP9Decoder_s));
 	if (pbi == NULL) {
@@ -7446,6 +7464,7 @@ static int ammvdec_vp9_probe(struct platform_device *pdev)
 	platform_set_drvdata(pdev, pdata);
 
 	pbi->platform_dev = pdev;
+	pbi->video_signal_type = 0;
 #if 1
 	if ((debug & IGNORE_PARAM_FROM_CONFIG) == 0 &&
 			pdata->config && pdata->config_len) {
@@ -7458,6 +7477,44 @@ static int ammvdec_vp9_probe(struct platform_device *pdev)
 		else
 			pbi->double_write_mode = double_write_mode;
 #endif
+		if (get_config_int(pdata->config, "HDRStaticInfo",
+				&vf_dp.present_flag) == 0
+				&& vf_dp.present_flag == 1) {
+			get_config_int(pdata->config, "mG.x",
+					&vf_dp.primaries[0][0]);
+			get_config_int(pdata->config, "mG.y",
+					&vf_dp.primaries[0][1]);
+			get_config_int(pdata->config, "mB.x",
+					&vf_dp.primaries[1][0]);
+			get_config_int(pdata->config, "mB.y",
+					&vf_dp.primaries[1][1]);
+			get_config_int(pdata->config, "mR.x",
+					&vf_dp.primaries[2][0]);
+			get_config_int(pdata->config, "mR.y",
+					&vf_dp.primaries[2][1]);
+			get_config_int(pdata->config, "mW.x",
+					&vf_dp.white_point[0]);
+			get_config_int(pdata->config, "mW.y",
+					&vf_dp.white_point[1]);
+			get_config_int(pdata->config, "mMaxDL",
+					&vf_dp.luminance[0]);
+			get_config_int(pdata->config, "mMinDL",
+					&vf_dp.luminance[1]);
+			vf_dp.content_light_level.present_flag = 1;
+			get_config_int(pdata->config, "mMaxCLL",
+					&content_light_level.max_content);
+			get_config_int(pdata->config, "mMaxFALL",
+					&content_light_level.max_pic_average);
+			vf_dp.content_light_level = content_light_level;
+			pbi->video_signal_type = (1 << 29)
+					| (5 << 26)	/* unspecified */
+					| (0 << 25)	/* limit */
+					| (1 << 24)	/* color available */
+					| (9 << 16)	/* 2020 */
+					| (16 << 8)	/* 2084 */
+					| (9 << 0);	/* 2020 */
+		}
+		pbi->vf_dp = vf_dp;
 	} else
 #endif
 	{
@@ -7466,6 +7523,8 @@ static int ammvdec_vp9_probe(struct platform_device *pdev)
 		pbi->vvp9_amstream_dec_info.rate = 30;*/
 		pbi->double_write_mode = double_write_mode;
 	}
+	video_signal_type = pbi->video_signal_type;
+
 #if 0
 	pbi->buf_start = pdata->mem_start;
 	pbi->buf_size = pdata->mem_end - pdata->mem_start + 1;
